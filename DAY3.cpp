822f372a2053aac50bbfd942fd30168a000f8308
RAJESH G R <rgr98624@gmail.com>
2026-06-18 11:07:33 +0530

    Implement number reversing feature
    Added functionality to reverse a number.

--- DAY3.cpp
+++ DAY3.cpp
@@ -34,4 +34,24 @@ OUTPUT
 Enter a number: 7
 Prime
 
-  
+2.REVERSING A NUMBER
+
+#include<iostream>
+ using namespace std;
+ 
+ int main(){
+    int n,reverse=0,digit;
+     
+    cout<<"Enter a number:";
+    cin>>n;
+     
+    while(n>0){
+        digit=n%10;
+        reverse=reverse*10+digit;
+        n=n/10;
+    }
+    cout<<reverse; 
+     
+    return 0;
+ }
+          
