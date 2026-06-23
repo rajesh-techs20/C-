b8fc9065ec2e1c133d4e85244d415b8c1f2f55b5
RAJESH G R <rgr98624@gmail.com>
2026-06-23 22:49:01 +0530

    Update day7.cpp

--- day7.cpp
+++ day7.cpp
@@ -23,10 +23,39 @@ int main(){
     cout<<"Max element is:"<<max;
     return 0;
     
-}
+;
 
 OUTPUT
 Enter the number of elements:5
 10 4 70 53 6
 Maximum elementMax element is:70
+
+
+
+MINIMUM ELEMENT
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[50],i,n;
+    
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    
+    cout<<"Enter elements:";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    };
+    int min=arr[0];
+    cout<<"minimum element";
+    for(i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<"Min element is:"<<min;
+    return 0;
+    
+};
+
 
