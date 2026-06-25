8cb73130d59eb40b497870794c6d1a760169e590
RAJESH G R <rgr98624@gmail.com>
2026-06-25 23:03:59 +0530

    Create day8.cpp

---
+++ day8.cpp
@@ -0,0 +1,23 @@
+AVERAGE OF ARRAY ELEMENTS
+
+ #include<iostream>
+ using namespace std;
+ int main() {
+     int arr[50],i,n,sum=0;
+     float average=0;
+     
+     cout<<"Enter the number of elements:";
+     cin>>n;
+     
+     cout<<"Enter elements:";
+     for(i=0;i<n;i++){
+         cin>>arr[i];
+     }
+     for(i=0;i<n;i++){
+         sum=sum+arr[i];
+         average=(float)sum/n;
+     }
+     cout<<"Average of array elements:"<<average;
+     return 0;
+     
+ }
